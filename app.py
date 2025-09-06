84b553da75377b19a1d302ac7eb2d88aea83621b
Hasnat4763 <abulhasnat5737@gmail.com>
2025-09-06 12:37:52 +0600

    Slack Bot

---
+++ app.py
@@ -0,0 +1,20 @@
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+load_dotenv(".env")
+
+
+
+bot_token = os.environ["SLACK_BOT_TOKEN"]
+app_token = os.environ["SLACK_APP_TOKEN"]
+
+app = App(token=bot_token)
+client = WebClient(token=bot_token)
+
+@app.command("/roaster_roastme")
+def roast_me(ack, respond, command):
+    ack()
+    user_id = command["user_id"]
+    respond(f"<@{user_id}> You are a Piece of Shit!")
